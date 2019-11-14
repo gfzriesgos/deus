3e6804292afc8456df553bd6b4a9b678632cbb76
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2019-11-14 16:30:41 +0100

    Added a test for the lahar show case

--- test_cmdexecution.py
+++ test_cmdexecution.py
@@ -347,10 +347,90 @@ class TestDeusCmdExecution(unittest.TestCase):
 
         self.assertEqual(input_n_buildings, output_n_buildings)
 
+class TestDeusCmdExecutionInEcuador(unittest.TestCase):
+    """
+    Test class to run deus in ecuador from cmd.
+    """
+
+    def test_execute_deus_in_ecuador_for_lahar(self):
+        schema = 'Mavrouli_et_al_2014'
+
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+
+        testinput_dir = os.path.join(current_dir, 'testinputs')
+        test_shakemap = os.path.join(
+            testinput_dir, 
+            'shakemap_lahar.xml'
+        )
+        test_exposure_file = os.path.join(
+            testinput_dir,
+            'exposure_model_lahar.json'
+        )
+        test_fragility_file = os.path.join(
+            testinput_dir,
+            'fragility_marvrouli.json'
+        )
+
+        output_dir = os.path.join(current_dir, 'testoutputs')
+
+        updated_exposure_output_filename = os.path.join(
+            output_dir,
+            'updated_exposure_lahar.json'
+        )
+        transition_output_filename = os.path.join(
+            output_dir,
+            'transitions_lahar.json'
+        )
+        loss_output_filename = os.path.join(
+            output_dir,
+            'losses_lahar.json'
+        )
+
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+
+        if os.path.exists(updated_exposure_output_filename):
+            os.unlink(updated_exposure_output_filename)
+
+        if os.path.exists(transition_output_filename):
+            os.unlink(transition_output_filename)
+
+        if os.path.exists(loss_output_filename):
+            os.unlink(loss_output_filename)
+
+        subprocess.run(
+            [
+                'python3',
+                'deus.py',
+                '--updated_exposure_output_file',
+                updated_exposure_output_filename,
+                '--transition_output_file',
+                transition_output_filename,
+                '--loss_output_file',
+                loss_output_filename,
+                test_shakemap,
+                test_exposure_file,
+                schema,
+                test_fragility_file,
+            ],
+            check=True,
+        )
+
+        input_n_buildings = get_n_buildings_by_cell_gid(
+            schema,
+            test_exposure_file
+        )
+        output_n_buildings = get_n_buildings_by_cell_gid(
+            schema,
+            updated_exposure_output_filename
+        )
+
+        self.assertEqual(input_n_buildings, output_n_buildings)
+
 
 class TestVolcanoCmdExecution(unittest.TestCase):
     '''
-    Test class to run deus as a command line tool.
+    Test class to run volcanus as a command line tool.
     '''
 
     def test_execute_volcano_for_ecuador(self):
