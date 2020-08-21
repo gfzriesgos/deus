5a44a93188942deeeee7a966465ae13220d090a4
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2020-08-21 07:45:17 +0200

    Added a deus executable variant for awi tsunami grids

---
+++ neptun.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+'''
+This is the very same as deus, but specialized
+to work with tsunami grids.
+The idea is the same as for volcanus to provide
+the deus interface with a specialized intensity provider.
+'''
+
+import argparse
+import glob
+import os
+
+import tellus
+
+import fragility
+import gpdexposure
+import intensityprovider
+import loss
+import rasterintensityprovider
+import shakemap
+
+
+def main():
+    '''
+    Runs the main method, which reads from
+    the files,
+    updates each exposure cell individually
+    and prints out all of the updated exposure cells.
+    '''
+    argparser = argparse.ArgumentParser(
+        description='Updates the exposure model and the damage ' +
+        'classes of the Buildings')
+    argparser.add_argument(
+        'intensity_file',
+        help='File with tsunami wave height data')
+    argparser.add_argument(
+        'exposure_file',
+        help='File with the exposure data')
+    argparser.add_argument(
+        'exposure_schema',
+        help='The actual schema for the exposure data')
+    argparser.add_argument(
+        'fragilty_file',
+        help='File with the fragility function data')
+    argparser.add_argument(
+        '--updated_exposure_output_file',
+        default='output_updated_exposure.json',
+        help='Filename for the output with the updated exposure data')
+    argparser.add_argument(
+        '--transition_output_file',
+        default='output_transitions.json',
+        help='Filename for the output with the transitions')
+    argparser.add_argument(
+        '--loss_output_file',
+        default='output_loss.json',
+        help='Filename for the output with the computed loss')
+    argparser.add_argument(
+        '--merged_output_file',
+        default='output_merged.json',
+        help='Filename for the merged output from all others')
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    loss_data_dir = os.path.join(current_dir, 'loss_data')
+    files = glob.glob(os.path.join(loss_data_dir, '*.json'))
+    loss_provider = loss.LossProvider.from_files(files, 'USD')
+
+    args = argparser.parse_args()
+
+    intensity_provider = rasterintensityprovider.RasterIntensityProvider.Shakemaps.from_file(
+        args.intensity_file, intensity='MWH', unit='m')
+    # ID for inundation (out of the maximum wave height)
+    intensity_provider = intensityprovider.AliasIntensityProvider(
+        intensity_provider,
+        aliases={
+            'ID': ['MWH'],
+        }
+    )
+    fragility_provider = fragility.Fragility.from_file(
+        args.fragilty_file).to_fragility_provider()
+    old_exposure = gpdexposure.read_exposure(args.exposure_file)
+
+    worker = tellus.Child(
+        intensity_provider,
+        fragility_provider,
+        old_exposure,
+        args.exposure_schema,
+        loss_provider,
+        args
+    )
+    worker.run()
+
+
+if __name__ == '__main__':
+    main()
