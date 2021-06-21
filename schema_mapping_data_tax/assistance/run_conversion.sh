f33a0916b3f58fff7ed620bf99db31c8be215c8f
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2021-06-21 12:04:52 +0200

    Simplifiyed license text

--- schema_mapping_data_tax/assistance/run_conversion.sh
+++ schema_mapping_data_tax/assistance/run_conversion.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-# Damage-Exposure-Update-Service
-# 
-# Command line program for the damage computation in a multi risk scenario
-# pipeline for earthquakes, tsnuamis, ashfall & lahars.
-# Developed within the RIESGOS project.
-#
 # Copyright © 2021 Helmholtz Centre Potsdam GFZ German Research Centre for Geosciences, Potsdam, Germany
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
 # 
-# http://www.apache.org/licenses/LICENSE-2.0
+# https://www.apache.org/licenses/LICENSE-2.0
 # 
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-#
-# Parts of this program were developed within the context of the
-# following publicly funded projects or measures:
-# -  RIESGOS: Multi-Risk Analysis and Information System 
-#    Components for the Andes Region (https://www.riesgos.de/en)
 
 ./recreate_json.py  Compatibility_Matrix_SARA_Suppasri.xlsx SARA_v1.0 SUPPASRI2013_v2.0 > Conversion_Matrix_SARA_Suppasri_completed.json
--- test_rasterintensityprovider.py
+++ test_rasterintensityprovider.py
@@ -1,23 +1,12 @@
 #!/usr/bin/env python3
 
-# Damage-Exposure-Update-Service
-# 
-# Command line program for the damage computation in a multi risk scenario
-# pipeline for earthquakes, tsnuamis, ashfall & lahars.
-# Developed within the RIESGOS project.
-#
 # Copyright © 2021 Helmholtz Centre Potsdam GFZ German Research Centre for Geosciences, Potsdam, Germany
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
 # 
-# http://www.apache.org/licenses/LICENSE-2.0
+# https://www.apache.org/licenses/LICENSE-2.0
 # 
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-#
-# Parts of this program were developed within the context of the
-# following publicly funded projects or measures:
-# -  RIESGOS: Multi-Risk Analysis and Information System 
-#    Components for the Andes Region (https://www.riesgos.de/en)
 
 """
 Testfile for testing
