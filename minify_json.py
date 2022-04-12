613acfea292bd575f492ad62960b07028696e2d0
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2022-04-12 11:11:44 +0200

    Fix error in minification script

--- minify_json.py
+++ minify_json.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
-"""Script to minify a json file (remove whitespace)."""
+"""Script to minify a json file inplace (remove whitespace)."""
 
 import json
 import sys
 
 
 def main():
+    """Run the main function to minify a json file."""
     if len(sys.argv) < 2:
         print("Usage: minify_json.py <file.json>", file=sys.stderr)
         exit(1)
@@ -17,7 +18,7 @@ def main():
         data = json.load(infile)
 
     with open(filename, "w") as outfile:
-        json.dumps(data, outfile)
+        json.dump(data, outfile)
 
 
 if __name__ == "__main__":
